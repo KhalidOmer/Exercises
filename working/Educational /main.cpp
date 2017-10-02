627aef9eea995a707c2d1f05fb5236c227b2be2d
KhalidOmer <khalid.hassan@aims.ac.rw>
2017-10-02 21:51:42 +0300

    working/Educational /

---
+++ working/Educational /main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char letter = 'B';
+    int number = 8;
+    string name = "Khalid1992";
+    double decimal = 7.77;
+    //cout << name << endl << name<< endl;
+    //cout << decimal;
+    string my_name = "My name is Khalid Hassan";
+    string age = "I am 24 years old";
+    string address = "I live in Khartoum, the capital of Sudan";
+    string personal_stuff = "I studied Physics and then I did a master's of Mathematical Sciences";
+    cout << my_name << endl << age << endl<< address<< endl<< personal_stuff;
+    return 0;
+}
