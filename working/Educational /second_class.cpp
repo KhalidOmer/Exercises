5daf307f41a8a61e4854e7a63c13ad946c15bf22
KhalidOmer <khalid.hassan@aims.ac.rw>
2017-10-05 20:27:57 +0300

    Tutorial 5th of October

---
+++ working/Educational /second_class.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+    string name = "Zack";
+    int age = 23;
+    cout << name << " is " << age << " years old. " << endl;
+
+    return 0;
