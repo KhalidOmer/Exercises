6b0529d3530c977d91449255219183bce47453de
KhalidOmer <khalid.hassan@aims.ac.rw>
2017-10-31 02:56:21 +0300

    Cleaning

---
+++ working/my_first_project/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string name = "Khalid";
+    int age = 23;
+    cout << name << " is " << age << " years old. " << endl;
+
+    string name_2;
+    int age_2;
+    cout << "Enter name_2: ";
+    cin >> name_2;
+    cout << endl << endl << " You entered: " << name_2 << endl;
+    cout << "Enter age_2: ";
+    cin >> age_2;
+    cout << endl << endl << " You entered: " << age_2 << endl;
+
+    char first_initial;
+    char last_initial;
+
+    cout << "Enter first initial: ";
+
+    cin >> first_initial;
+    cout<< endl << endl << " enter second initial: ";
+    cin >> last_initial;
+    cout<< endl<< endl<< "your initials are "<< first_initial << last_initial<< endl; o
+    return 0;
+}
